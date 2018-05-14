4642d59884e064751529d695a2c803f59a04d8b6
Hua Do Gia Khanh <giakhanh2487@gmail.com>
2018-05-15 07:15:10 +0800

    Added spec for-sum-1st-degree-refs

--- src/compiler.spec.js
+++ src/compiler.spec.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { default as compiler, cellToPublicSection, CodeGen } from './compiler';
+import { default as compiler, cellToFunModel, CodeGen } from './compiler';
 import {Node} from "excel-formula-ast/index";
 
 describe('compiler', () => {
@@ -41,7 +41,7 @@ describe('formulaToCode', () => {
       formula: 'NOW()'
     };
 
-    const actual = cellToPublicSection(codeGen, cell);
+    const actual = cellToFunModel(codeGen, cell);
     expect(actual).to.deep.equal({
       name: 'funSheet1$A1',
       address: 'Sheet1!A1',
@@ -189,7 +189,41 @@ describe('CodeGen', () => {
     });
 
     it('generates function with one cell param, which is contains a formula', () => {
+      /* TODO Optimize by immediately resolve constant params at compile time (ie, SUM(1,2,3) => 6)
+         vs SUM(1, 2, A1) which cannot be reduced
+         However, optimization should occur from bottom to top as A1 could be resolve to a constant (e.g. 3)
+       */
+      const mockWorkBook = {
+        Sheets: {
+          'Sheet1': {
+            'B4': {
+              f: 'SUM(1,2,3)',
+              format: 'general',
+              dataType: 'number'
+            }
+          }
+        }
+      };
+      codeGen = new CodeGen(mockWorkBook);
+      codeGen.setCurrentSheet('Sheet1');
 
+      const node = {
+        type: 'function',
+        name: 'SUM',
+        arguments: [
+          {
+            type: 'cell',
+            key: 'B4',
+            refType: 'relative'
+          }]
+      };
+      codeGen.enterFunction(node);
+      codeGen.enterCell(node.arguments[0]);
+      codeGen.exitCell(node.arguments[0]);
+      codeGen.exitFunction(node);
+
+      const actual = codeGen.jsCode();
+      expect(actual).to.equal('EXCEL.SUM($$("Sheet1!B4"))');
     });
 
     it('generates function with one cell-range param', () => {
